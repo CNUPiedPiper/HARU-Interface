32ae615a792ab4c023379b611ea057225413654d
yoon-jae <yoonjae.cho92@gmail.com>
2017-09-13 00:12:35 +0900

    Upload base HARU-Interface

---
+++ src/main.py
@@ -0,0 +1,71 @@
+#-*- coding: utf-8 -*- 
+import configparser
+from detector import hotword
+from recorder import recorders
+from speech_recognition import transcribe_streaming
+from led_controller import Led_controller
+from text2speech import text2speech
+
+# Haru main class.
+class Main:
+    def __init__(self):
+        self.config = configparser.RawConfigParser()
+        self.config.read('config.ini')
+        naver_id = self.config.get('NAVER', 'id')
+        naver_secret = self.config.get('NAVER', 'secret')
+
+        self.detector = hotword.hotword()
+        self.rec = recorder.Recorder()
+        self.speaker = text2speech.Text2Speech(naver_id, naver_secret)
+        
+        self.led = Led_controller()
+        self.led.start()
+        self.speaker.speak(u'안녕하세요')
+        self.speaker.speak(u'하루를 시작합니다..')
+
+    def request_api(self, sentence):
+        return "response json object"
+
+    def preprocessing(self, response):
+        return "Answer text"
+
+    def main_flow(self):
+        print('[HARU] In Main flow..')
+        print('[HARU] Recording now.. Ask a question now') 
+        self.led.turn_on()
+        
+        # Record user's order sentence.
+        audio_buffer = self.rec.record_audio()
+
+        print('[HARU] Now transcribe the audio buffer to text')
+        self.led.turn_off()
+
+        # Transcribing audio buffer streaming to korean using Google Speech Recognition api.
+        sentence = transcribe_streaming.transcribe_streaming(audio_buffer)
+        #sentence = u'오늘 날씨는 어때'
+        #sentence = u'오늘 이슈는 뭐야'
+        #sentence = u'지금 몇시야'
+        #sentence = u'이 노래가 뭐지'
+
+        self.rec.close_buf()
+        self.led.turn_on()
+
+        # Request for classifying user's order sentence.
+        response = self.request_api(sentence)
+        answer_text = self.preprocessing(response)
+
+        # Speak the answer text using Naver TTS api.
+        self.speaker.speak(answer_text)
+        self.led.turn_off()
+        
+        # Call run funciton again.
+        self.run()
+
+    def run(self):
+        # Detecting wake-up word.
+        self.detector.start_detection(self.main_flow)
+
+if __name__ == "__main__":
+    print('[HARU] Starting the HARU')
+    haru = Main()
+    haru.run()
---
+++ src/recorder/recorder.py
@@ -0,0 +1,93 @@
+import pyaudio
+import wave
+from array import array
+import os
+import sys
+sys.path.append('/home/pi/prototype/src/speech_recognition')
+from transcribe_streaming import transcribe_streaming
+
+
+# Set device number with get_dev_index.py.
+DEVICE_NUMBER = 1
+
+class Recorder:
+	def __init__(self):
+		self.THRESHOLD = 500
+		self.FORMAT = pyaudio.paInt16
+		self.CHANNELS = 1
+		self.RATE = 44100
+		self.CHUNK = 8192 # int(RATE / 10)
+		self.MINIMUM_RECORD_SECONDS = 3 # seconds (approximate)
+		self.f = None
+
+	def record_audio(self):
+		def is_silent(data):
+			# When it's silent, max(data) tend to be 100~200
+			return max(data) < self.THRESHOLD
+	
+		p = pyaudio.PyAudio()
+	
+		stream = p.open(
+			format=self.FORMAT,
+			channels=self.CHANNELS,
+			rate=self.RATE,
+			input=True,
+			frames_per_buffer=self.CHUNK,
+			input_device_index=DEVICE_NUMBER
+		)
+
+		print("[HARU] Recording audio...")
+
+		frames = array('h')
+		minimum = 0
+		silent_counter = 0
+		
+		while True:
+			data = array('h', stream.read(self.CHUNK))
+
+			# Record sounds while at least 3 seconds
+			if minimum < int(self.RATE / self.CHUNK * self.MINIMUM_RECORD_SECONDS):
+				frames.extend(data)
+				minimum = minimum + 1
+
+			# After 3 seconds
+			else:
+				silent = is_silent(data)
+
+				# If it is noisy
+				if not silent:
+					frames.extend(data)
+					silent_counter = 0
+
+				# If it is silent, wait a second more
+				elif silent and silent_counter < 5: # 1 second (approximate)
+					frames.extend(data)
+					silent_counter = silent_counter + 1
+
+				# After a second, finish the recording
+				else:
+					break
+
+		print("[HARU] Recording is done")
+
+		stream.stop_stream()
+		stream.close()
+		#p.terminate()
+
+		self.f = open(".sound.raw", "w+")
+		frames.tofile(self.f)
+		self.f.write('\0')
+		self.f.seek(0)
+		return self.f
+	
+	def close_buf(self):
+		self.f.seek(0)
+		self.f.truncate()
+		self.f.close()
+
+if __name__ == "__main__":
+	r = Recorder()
+	f = r.record_audio()
+	sen = transcribe_streaming(f)
+        print(sen)
+	r.close_buf()
